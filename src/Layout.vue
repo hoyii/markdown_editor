2e291d30519d8feccb6c2e3aa79caec8f9e0e68e
hoyii <812473076@qq.com>
2025-03-02 23:03:28 +0800

    添加编辑器和预览组件，重构应用布局

---
+++ src/Layout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="display: flex">
+    <Editor />
+    <Preview />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Editor from './MEditor/Editor.vue';
+import Preview from './MPreview/Preview.vue';
+</script>
---
+++ src/MEditor/Editor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div ref="editor" id="editor" style="flex: 1"></div>
+</template>
+
+<script setup lang="ts">
+import { EditorView, basicSetup } from 'codemirror';
+import { markdown } from '@codemirror/lang-markdown';
+import { languages } from '@codemirror/language-data';
+import { onMounted, ref } from 'vue';
+
+const editor = ref(null);
+
+onMounted(() => {
+  if (editor.value) {
+    const view = new EditorView({
+      parent: editor.value,
+      doc: "# Hello\n\n```javascript\nlet x = 'y'\n```",
+      extensions: [basicSetup, markdown({ codeLanguages: languages })]
+    });
+    // 设置编辑器的样式
+    view.dom.style.height = `${window.innerHeight}px`;
+    view.scrollDOM.style.overflow = 'auto';
+  }
+});
+</script>
+
+<style scoped></style>

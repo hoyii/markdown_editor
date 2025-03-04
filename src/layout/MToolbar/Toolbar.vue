7a25eab5371daea721581b03269448d761ebef2f
hoyii <812473076@qq.com>
2025-03-04 14:29:15 +0800

    添加更多工具栏按钮以支持 Markdown 语法插入

--- src/layout/MToolbar/Toolbar.vue
+++ src/layout/MToolbar/Toolbar.vue
@@ -7,20 +7,51 @@
     <button @click="insertSyntax('**', 1)">
       <Italic :size="14" />
     </button>
+    <button @click="insertSyntax('~~~~', 2)">
+      <Strikethrough :size="14" />
+    </button>
     <button @click="insertSyntax('- ')">
       <List :size="14" />
     </button>
+    <button @click="insertSyntax('1. ')">
+      <ListOrdered :size="14" />
+    </button>
     <button @click="insertSyntax('[]()', 1)">
       <Link :size="14" />
+    </button>
+    <button @click="insertSyntax('![]()', 2)">
+      <Image :size="14" />
+    </button>
+    <button @click="insertSyntax('***')">
+      <PencilLine :size="14" />
+    </button>
+    <button @click="insertSyntax('> ')">
+      <Quote :size="14" />
     </button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { List, Link, Heading, Bold, Italic } from 'lucide-vue-next';
+import {
+  Heading,
+  Bold,
+  Italic,
+  Strikethrough,
+  List,
+  ListOrdered,
+  Link,
+  Image,
+  PencilLine,
+  Quote
+} from 'lucide-vue-next';
 import $bus from '../../state/mitt';
 
-// 插入标题、列表等语法
+/**
+ * Inserts a given syntax at the current cursor position in the editor.
+ *
+ * @param {string} syntax - The syntax string to be inserted.
+ * @param {number} [cursor_offset=syntax.length] - The offset position of the cursor after insertion. Defaults to the length of the syntax string.
+ */
 const insertSyntax = (syntax: string, cursor_offset: number = syntax.length) => {
   $bus.emit('insertSyntax', [syntax, cursor_offset]);
 };

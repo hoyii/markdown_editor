495825fabfd8e2a9c0c00d2db1d3fe3426445b0c
hoyii <812473076@qq.com>
2025-03-04 23:22:59 +0800

    新增初始化文本

--- src/state/data.ts
+++ src/state/data.ts
@@ -1,3 +1,58 @@
-const markdownData = '# hello';
+// 模拟多行文本
+const markdownData = `## 😲 markdown_editor
+
+Markdown Editor for Vue3, developed in jsx and typescript, support different themes、beautify content by prettier.
+
+### 🤖 Base
+
+**bold**, _italic_, ~~line-through~~, \`inline code\`, [link](https://github.com/imzbf)
+
+> quote: I Have a Dream
+
+1. So even though we face the difficulties of today and tomorrow, I still have a dream.
+2. It is a dream deeply rooted in the American dream.
+3. I have a dream that one day this nation will rise up.
+
+- [ ] Friday
+- [ ] Saturday
+- [x] Sunday
+
+![Picture](https://imzbf.github.io/md-editor-rt/imgs/mark_emoji.gif)
+
+## 🤗 Code
+
+\`\`\`vue
+<template>
+  <MdEditor v-model="text" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { MdEditor } from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+const text = ref('Hello Editor!');
+</script>
+\`\`\`
+
+## 🖨 Text
+
+The Old Man and the Sea served to reinvigorate Hemingway's literary reputation and prompted a reexamination of his entire body of work.
+
+## 📈 Table
+
+| THead1          |      THead2       |           THead3 |
+| :-------------- | :---------------: | ---------------: |
+| text-align:left | text-align:center | text-align:right |
+
+## 🧬 Diagram
+
+\`\`\`mermaid
+flowchart TD
+  Start --> Stop
+\`\`\`
+
+## ☘️ em...
+`;
 
 export default markdownData;
